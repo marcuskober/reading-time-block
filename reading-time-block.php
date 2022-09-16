cd582470286ab2c71716dab977969ff14b8fd282
Marcus Kober <marcus.kober@gmail.com>
2022-09-16 16:15:03 +0200

    :rocket: INITIAL COMMIT

---
+++ reading-time-block.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Reading time block
+ * Plugin URI: https://marcuskober.de
+ * Description: A Gutenberg block to display reading time
+ * Version: 0.0.1
+ * Author: Marcus Kober
+ * Author URI: https://marcuskober.de
+ */
