052678fac24a0f550cdf4cf44eba2d9806968ff1
Marcus Kober <marcus.kober@gmail.com>
2022-09-19 12:19:49 +0200

    :package: NEW: Add internationalization

--- reading-time-block.php
+++ reading-time-block.php
@@ -3,11 +3,13 @@
  * Plugin Name: Reading time block
  * Plugin URI: https://marcuskober.de
  * Description: A Gutenberg block to display reading time of a post or page.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Marcus Kober
  * Author URI: https://marcuskober.de
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: marcuskober-reading-time
+ * Domain Path: /languages
  */
 
 namespace Marcuskober\ReadingTimeBlock;
--- src/edit.js
+++ src/edit.js
@@ -3,7 +3,7 @@ import { PanelBody, TextControl } from '@wordpress/components'
 import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element'
 
-const edit = ({attributes, setAttributes, props}) => {
+const edit = ({attributes, setAttributes}) => {
   const blockProps = useBlockProps();
 
   blockProps.className +=  ` has-text-align-${ attributes.textAlign }`;
@@ -13,13 +13,13 @@ const edit = ({attributes, setAttributes, props}) => {
       <InspectorControls>
         <PanelBody title={__('Settings', 'marcuskober/reading-time')} initialOpen={ true }>
           <TextControl
-            label={__('Text template', 'wpcrman')}
+            label={__('Text template', 'marcuskober-reading-time')}
             value={attributes.textTemplate}
             onChange={value => setAttributes({textTemplate: value})}
           />
 
           <TextControl
-            label={__('Words per minute', 'wpcrman')}
+            label={__('Words per minute', 'marcuskober-reading-time')}
             type="number"
             value={attributes.wordsPerMinute}
             onChange={value => setAttributes({wordsPerMinute: parseInt(value)})}
